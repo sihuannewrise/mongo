c72990a2a1b791a826abde1e72ab87f0572751c3
Sihuan Newrise <sihuannewrise@gmail.com>
2025-09-19 14:33:24 +0300

    mongodb conn

--- demo_mongodb_test.py
+++ demo_mongodb_test.py
@@ -1,13 +1,13 @@
 import pymongo
 
-myclient = pymongo.MongoClient("mongodb://mongodbuser:aw3se4dr5@mongodb.almeling.ru:433/")
+myclient = pymongo.MongoClient("mongodb://mongodbuser:aw3se4dr5@mongodb.almeling.ru:27017/")
 
 mydb = myclient["mydatabase"]
 mycol = mydb["customers"]
 
-# mydict = { "name": "Matwey", "address": "Highlands 23" }
+mydict = { "name": "Motya", "address": "Matenburg 14" }
 
-# x = mycol.insert_one(mydict)
-# print(x.inserted_id)
+x = mycol.insert_one(mydict)
+print(x.inserted_id)
 
 print(mycol)
