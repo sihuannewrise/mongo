e66e6d41249e3a5e9d977beb09ae4757ea507a53
sihuannewrise <sihuannewrise@gmail.com>
2025-09-22 21:55:40 +0300

    mongo conn

--- demo_mongodb_test.py
+++ demo_mongodb_test.py
@@ -12,12 +12,11 @@ print(x.inserted_id)
 
 print(mycol)
 
-# docker exec -it mongodb mongosh -u mongodbuser -p aw3se4dr5
-# mongosh --host mogdodb.almeling.ru --username mongodbuser --password aw3se4dr5 --authenticationDatabase knhdb
-# mongosh --host mogdodb.almeling.ru -u mongodbuser -p aw3se4dr5
-# mongosh mongodb://mongodbuser:aw3se4dr5@mongodb.almeling.ru:27017/mydatabase -u mongodbuser -p aw3se4dr5
-
 # start mongo shell
-# docker exec -it mongodb mongosh -u <mongodbuser> -p <password>
+# docker exec -it mongodb mongosh -u mongodbuser -p aw3se4dr5
+# mongosh --host almeling.ru --username mongodbuser --password aw3se4dr5 --authenticationDatabase knhdb
+# mongosh --host almeling.ru -u mongodbuser -p aw3se4dr5
+# mongosh mongodb://mongodbuser:aw3se4dr5@almeling.ru:27017 -u mongodbuser -p aw3se4dr5
+
 # db.auth({user: "mongodbuser", passwordPrompt()})
 # db.auth({user: "mongodbuser", pwd: "aw3se4dr5"})
