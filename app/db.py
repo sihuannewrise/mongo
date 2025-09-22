4afddbdeafc4e3d9bf6c9bdd007357274e7bcb28
sihuannewrise <sihuannewrise@gmail.com>
2025-09-22 21:57:14 +0300

    db conn

--- app/db.py
+++ app/db.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://mongodbuser:aw3se4dr5@mongodb.almeling.ru:27017")
+client = MongoClient("mongodb://mongodbuser:aw3se4dr5@almeling.ru:27017/")
 db = client["library_management"]
 students_collection = db["students"]
