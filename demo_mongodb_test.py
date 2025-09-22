01441c6acdb188849d6709b111c7d3b5802fcb3b
Sihuan Newrise <sihuannewrise@gmail.com>
2025-09-22 16:06:10 +0300

    pymongo

--- demo_mongodb_test.py
+++ demo_mongodb_test.py
@@ -1,6 +1,6 @@
 import pymongo
 
-myclient = pymongo.MongoClient("mongodb://mongodbuser:aw3se4dr5@mongodb.almeling.ru:27017/")
+myclient = pymongo.MongoClient("mongodb://mongodbuser:aw3se4dr5@92.38.197.198:27017/")
 
 mydb = myclient["mydatabase"]
 mycol = mydb["customers"]
@@ -11,3 +11,14 @@ x = mycol.insert_one(mydict)
 print(x.inserted_id)
 
 print(mycol)
+
+# docker exec -it mongodb mongosh -u mongodbuser -p aw3se4dr5
+# mongosh --host mogdodb.almeling.ru --username mongodbuser --password aw3se4dr5 --authenticationDatabase knhdb
+# mongosh --host mogdodb.almeling.ru -u mongodbuser -p aw3se4dr5
+# mongosh mongodb://mongodbuser:aw3se4dr5@mongodb.almeling.ru:27017/mydatabase -u mongodbuser -p aw3se4dr5
+
+# start mongo shell
+# docker exec -it mongodb mongosh -u <mongodbuser> -p <password>
+# db.auth({user: "mongodbuser", passwordPrompt()})
+# db.auth({user: "mongodbuser", pwd: "aw3se4dr5"})
+
