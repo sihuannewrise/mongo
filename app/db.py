49da09d34ad9c2a142e4ea2039edd1dff6431197
Sihuan Newrise <sihuannewrise@gmail.com>
2025-09-23 13:58:26 +0300

    fastapi test

--- app/db.py
+++ app/db.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 
 client = MongoClient("mongodb://mongodbuser:aw3se4dr5@almeling.ru:27017/")
-db = client["library_management"]
+db = client["knhdb"]
 students_collection = db["students"]
